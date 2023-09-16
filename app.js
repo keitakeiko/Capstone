b68ad920f3cd32c00676d503bb8d603c38fd9aa0
Keiko <keitakeiko@hotmail.com>
2023-09-16 16:24:00 +0800

    feat: add handebars

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
 
 const PORT = 3000
 
+// set view engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// 所有路由都會先經過 app.use
+app.use(express.static('public'))
+
+
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('layouts/main')
 })
 
+
+// 監聽函式
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
