f77796b24df957a9c33c074fe86a7bb1d5ca4973
Keiko <keitakeiko@hotmail.com>
2023-09-27 00:38:30 +0800

    feat: add signup page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,10 +1,56 @@
 const bcrypt = require('bcryptjs')
-const countryCodes = require('country-codes-list')
 
-const { User, sequelize } = require('../models')
+const { User, Class, Enrollment, sequelize } = require('../models')
 const { getAbbreviationCountry } = require('../helpers/handlebars-helpers')
+const BCRYPT_SALT_LENGTH = 10
 
 const userController = {
+  getSignUPpage: (req, res) => {
+    res.render('users/signup')
+  },
+  postSignUp: async (req, res) => {
+    const DEFAULT_PASSWORD = '12345678'
+    const salt = bcrypt.genSaltSync(BCRYPT_SALT_LENGTH)
+
+    await queryInterface.bulkInsert('Users', [{
+      name: 'root',
+      account: 'root',
+      email: 'root@example.com',
+      password: bcrypt.hashSync(DEFAULT_PASSWORD, salt),
+      about_me: faker.lorem.sentence(INTRODUCTION_LENGTH),
+      avatar: `https://loremflickr.com/g/300/300/pomeranian,dog/?lock=100`,
+      nation: NATION,
+      role: 'admin',
+      created_at: new Date(),
+      updated_at: new Date()
+    },
+    ...Array.from({ length: STUDENT_AMOUNT }, (_, i) => ({
+      name: `user${ i + 1 }`,
+      account: `user${ i + 1 }`,
+      email: `user${ i + 1 }@example.com`,
+      password: bcrypt.hashSync(DEFAULT_PASSWORD, salt),
+      about_me: faker.lorem.sentence(INTRODUCTION_LENGTH),
+      avatar: `https://loremflickr.com/g/300/300/pomeranian,dog/?lock=${ i + 1 }`,
+      nation: NATION,
+      role: 'user', // student
+      created_at: new Date(),
+      updated_at: new Date()
+    })),
+    // 每個使用者有至少 2 頁（10 篇）老師可以選擇
+    ...Array.from({ length: TEACHER_AMOUNT }, (_, i) => ({
+      name: `user${ i + TEACHER_ID_START }`,
+      account: `user${ i + TEACHER_ID_START }`,
+      email: `user${ i + TEACHER_ID_START }@example.com`,
+      password: bcrypt.hashSync(DEFAULT_PASSWORD, salt),
+      about_me: faker.lorem.sentence(INTRODUCTION_LENGTH),
+      avatar: `https://loremflickr.com/g/300/300/pomeranian,dog/?lock=${ i + 100 }`,
+      nation: NATION,
+      role: 'teacher',
+      created_at: new Date(),
+      updated_at: new Date()
+    }))
+  ])
+  },
   getHomeTeachers: (req, res) => res.render('index'),
   getUserPage: (req, res) => {
     return res.render('users/userPage')
@@ -26,8 +72,6 @@ const userController = {
     return res.render('users/reserve-class')
   },
    getCommentPage: (req, res) => res.render('users/commentPage')
-    
-  
 }
 
 module.exports = {
