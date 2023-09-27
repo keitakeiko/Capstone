ea8778c63aa7aa0079d0ee12a330a9f61bff914a
Keiko <keitakeiko@hotmail.com>
2023-09-27 23:35:07 +0800

    feat: flash msg & signup verification

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express')
 const exphbs = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 const hbsHelper = require('./helpers/handlebars-helpers')
 const routes = require('./routes') // 預設會找底下 index.js
 const app = express()
 const PORT = process.env.PORT || 3000
+const SESSION_SECRET = 'whisper crag'
 
 // set view engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main', helpers: hbsHelper }))
@@ -16,6 +19,13 @@ app.set('view engine', 'handlebars')
 // 所有路由都會先經過 app.use
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: SESSION_SECRET, reserve: false, saveUninitialized: false }))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 app.use(routes)
 
 // 監聽函式
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router()
 
 const admin = require('./modules/admin')
 const users = require('./modules/users')
+const { generalErrorHandler } = require('../middleware/error-handler')
 const { userController } = require('../controllers/user-controller')
 
 router.use('/users', users)
 router.use('/admin', admin)
 router.use('/', userController.getHomeTeachers)
+router.use('/', generalErrorHandler)
 
 module.exports = router
